737dcd03f0780abf9ff84aa9b38a4e702df90b31
hinzdc <devilcyber33@gmail.com>
2024-01-06 11:16:58 +0700

    Update WindowsLoginUnlocker.bat

--- get/WindowsLoginUnlocker.bat
+++ get/WindowsLoginUnlocker.bat
@@ -1,5 +1,5 @@
 @echo off
-title get Windows Login Unlocker ^| hinzdc
+title Windows Login Unlocker ^| hinzdc
 echo.
 echo                      ENABLING ADMINISTRATOR RIGHTS...
 echo.
@@ -13,19 +13,31 @@ echo		 	           Please Wait...
     CD /D "%~dp0"
 
 cls
-mkdir c:\temp
+IF NOT EXIST "C:\temp" (
+    mkdir "C:\temp"
+    echo Folder berhasil dibuat.
+) ELSE (
+    echo Folder sudah ada.
+)
 cd /d C:\temp
 set url=https://github.com/hinzdc/hinzdc.github.io/raw/main/dl/TBWinPE.exe
 set url2=https://drive.usercontent.google.com/download?id=15XvDZSbyiyHHPdkLgF3PGzLZm0cTs-11^&export=download^&confirm=t
 set output=C:\temp\TBWinPE.exe
 set output2=C:\temp\boot.wim
-
+timeout /T 3
+cls
 echo.
 echo Downloading Files Required..
 powershell.exe -Command "Invoke-RestMethod -Useb '%url%' -OutFile '%output%'"
 powershell.exe -Command "Invoke-RestMethod -Useb '%url2%' -OutFile '%output2%'"
 
 mkdir bcd_backup
+IF NOT EXIST "C:\temp\bcd_backup" (
+    mkdir "C:\temp\bcd_backup"
+    echo Folder berhasil dibuat.
+) ELSE (
+    echo Folder sudah ada.
+)
 del %~dp0\bcd_backup\BCD
 bcdedit /export %~dp0\bcd_backup\BCD
 tbwinpe.exe /bootwim %~dp0\boot.wim /quiet /force
